1b13656f1d555fcfdbdb8287efb4f62df8a9164b
Thomas Wong <thomaskf@gmail.com>
2026-01-12 14:30:17 +1100

    set conditionSurv to false for the class MosseTreeLikelihoodBufferedMT

--- src/mosse/MosseTreeLikelihoodBufferedMT.java
+++ src/mosse/MosseTreeLikelihoodBufferedMT.java
@@ -135,7 +135,7 @@ public class MosseTreeLikelihoodBufferedMT extends MosseTreeLikelihoodBuffered i
 	                double[] partials = new double[singlePartialSize];
 	                System.arraycopy(partialsAllPatterns, startPos, partials, 0, singlePartialSize);
 	
-	                boolean conditionSurv = true;
+	                boolean conditionSurv = false; // runtime exception exists when this value is true;
 	                double pll = makeRootFuncMosse(numRateBins_l, dx_l, resolution, partials, conditionSurv);
 	                patternLogLikelihoods[p] = pll;
 	        	}
@@ -146,7 +146,7 @@ public class MosseTreeLikelihoodBufferedMT extends MosseTreeLikelihoodBuffered i
 	                double[] partials = new double[singlePartialSize];
 	                System.arraycopy(partialsAllPatterns, startPos, partials, 0, singlePartialSize);
 	
-	                boolean conditionSurv = true;
+	                boolean conditionSurv = false; // runtime exception exists when this value is true;
 	                double pll = makeRootFuncMosse(numRateBins_l, dx_l, resolution, partials, conditionSurv);
 	                patternLogLikelihoods[p] = pll;
 	            });
